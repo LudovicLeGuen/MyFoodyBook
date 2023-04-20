555d2182a2780d83a6da4281d78b7f721a2e2535
LudovicLeGuen <>
2023-04-20 19:24:43 +0000

    add base, index and recipe_details htmls and connect them to views.py and urls.py

--- publish/views.py
+++ publish/views.py
@@ -3,8 +3,8 @@ from django.views import generic
 from .models import Recipe
 
 
-class RecipeList(generic.ViewList):
+class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name - 'index.html'
+    template_name = 'index.html'
     paginate_by = 6
