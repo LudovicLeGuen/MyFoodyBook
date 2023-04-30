3850f1534a849181718525c15b75925c313ef453
LudovicLeGuen <>
2023-04-30 14:13:28 +0000

    Add register view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from users import views
 from django.urls import path
+from .views import profile, RegisterView 
 
 
 urlpatterns = [
     path('', views.profile, name='users-profile'),
+    path('register/', RegisterView.as_view(), name='users-register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+# copied from Hana Belay https://dev.to/earthcomfy/creating-a-django-registration-login-app-part-ii-3k6
+from django.shortcuts import render, redirect 
+from django.contrib import messages
+from django.views import View
+from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
 
+
+class RegisterView(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'users/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome to the Foody Book {username}! Your account is created.')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
 
 @login_required
 def profile(request):
