adae983a7ddf1330340cd2511f3305f6a332a06e
LudovicLeGuen <>
2023-05-14 09:51:13 +0000

    Add slug field to Profile model and style index.html

--- users/models.py
+++ users/models.py
@@ -7,9 +7,8 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = CloudinaryField(
-        'image',
-        default='https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Ddefault%2Bavatar&psig=AOvVaw3je7XgNuEkdhlBAActQE2C&ust=1683040511213000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCKi_-NK01P4CFQAAAAAdAAAAABAE')
+    slug = models.SlugField(max_length=200, unique=True, null=True)
+    avatar = CloudinaryField('image', default='placeholder')
     bio = models.TextField()
     favorite_food = models.TextField()
     nationality = models.TextField()
