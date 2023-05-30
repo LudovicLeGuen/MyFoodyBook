9112e6f8c02d8999361599d769c9d33632bd74d8
LudovicLeGuen <>
2023-05-30 18:49:49 +0000

    Fix trial for deploy with a new css file

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,9 @@ from .views import profile, RegisterView, my_profile, ShowUserProfile
 urlpatterns = [
     path('', views.my_profile, name='my-profile'),
     path('profile/<int:user_id>', views.profile, name='users-profile'),
-    path('show_profile/<int:pk>', ShowUserProfile.as_view(), name='show-profile'),
-    path('userslist/', views.show_all_users, name='users-list'),    
+    path('show_profile/<int:pk>',
+         ShowUserProfile.as_view(), name='show-profile'),
+    path('userslist/', views.show_all_users, name='users-list'),
     path('register/', RegisterView.as_view(), name='users-register'),
 
 ]
